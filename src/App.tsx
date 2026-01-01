0baa5a8c534dff26af3cf9df04b134d632b6365e
notsooryaa <stunsooryaa@gmail.com>
2026-01-01 22:02:41 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+    <div className="flex items-center justify-center bg-[#FFFFFF] h-screen">
+      <h1 className="text-3xl font-bold underline">Task Manager</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
